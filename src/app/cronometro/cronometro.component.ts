28cecbaa4c3ff4282bda28c135e901fc1166ecaa
Flavio Milani <milanidesign7@gmail.com>
2022-04-19 23:38:13 -0300

    Controle de botões no cronometro

--- src/app/cronometro/cronometro.component.ts
+++ src/app/cronometro/cronometro.component.ts
@@ -13,21 +13,37 @@ export class CronometroComponent implements OnInit, OnDestroy {
   timeLeft: number = 0;
   interval: any;
 
+  hideIniciar: boolean = false;
+  hidePausar: boolean = false;
+  hideRetomar: boolean = false;
+  hideReiniciar: boolean = false;
+  hideVolta: boolean = false;
+
   minutes: any = '00';
   seconds: any = '00';
   milliseconds: any = '00';
 
-  constructor(public timer: TimerService) { }
+  constructor(public ts: TimerService) { }
 
   ngOnDestroy(): void {
     this.pause();
   }
 
   ngOnInit(): void {
+    this.hideIniciar = true;
+    this.hideRetomar = true;
+    this.hidePausar = true;
+    this.hideReiniciar = true;
+    this.hideVolta = true;
   }
 
   start() {
     if (!this.interval) {
+      this.hideIniciar = false;
+      this.hidePausar = false;
+      this.hideRetomar = true;
+      this.hideReiniciar = true;
+      this.hideVolta = false;
       this.interval = setInterval(() => {
         this.timeLeft++;
       }, 100);
@@ -36,14 +52,26 @@ export class CronometroComponent implements OnInit, OnDestroy {
 
   pause() {
     if (this.interval) {
+      this.hideRetomar = false;
+      this.hidePausar = true;
+      this.hideReiniciar = false;
+      this.hideVolta = true;
       clearInterval(this.interval);
       this.interval = undefined;
     }
   }
 
+  reset() {
+    this.hideIniciar = true;
+    this.hideReiniciar = true;
+    this.hideRetomar = true;
+    this.hideVolta = true;
+    this.timeLeft = 0;
+    this.laps = [];
+  }
+
   marcarVolta() {
     this.laps.push(this.timeLeft);
-    console.log(this.timeLeft);
   }
 
 }
