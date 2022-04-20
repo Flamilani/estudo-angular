d40044b7769f48bfbd1dab26759bb9ce722e2237
Flavio Milani <milanidesign7@gmail.com>
2022-04-20 19:48:45 -0300

    projeto parcial

--- src/app/cronometro/cronometro.component.ts
+++ src/app/cronometro/cronometro.component.ts
@@ -13,15 +13,6 @@ export class CronometroComponent implements OnInit, OnDestroy {
   timeLeft: number = 0;
   interval: any;
 
-  totalLaps: number = this.laps.length;
-  hideStart: boolean = false;
-  hidePause: boolean = false;
-  hideResume: boolean = false;
-  hideReset: boolean = false;
-  hideLaps: boolean = false;
-  titleLaps: boolean = false;
-  displayLaps: string = '';
-
   minutes: any = '00';
   seconds: any = '00';
   milliseconds: any = '00';
@@ -33,23 +24,11 @@ export class CronometroComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.hideStart = true;
-    this.hideResume = true;
-    this.hidePause = true;
-    this.hideReset = true;
-    this.hideLaps = true;
-    this.titleLaps = true;
-    this.displayLaps = 'none';
 
   }
 
   start() {
     if (!this.interval) {
-      this.hideStart = false;
-      this.hidePause = false;
-      this.hideResume = true;
-      this.hideReset = true;
-      this.hideLaps = false;
       this.interval = setInterval(() => {
         this.timeLeft++;
       }, 100);
@@ -58,29 +37,17 @@ export class CronometroComponent implements OnInit, OnDestroy {
 
   pause() {
     if (this.interval) {
-      this.hideResume = false;
-      this.hidePause = true;
-      this.hideReset = false;
-      this.hideLaps = true;
       clearInterval(this.interval);
       this.interval = undefined;
     }
   }
 
   reset() {
-    this.hideStart = true;
-    this.hideReset = true;
-    this.hideResume = true;
-    this.hideLaps = true;
     this.timeLeft = 0;
-    this.titleLaps = true;
-    this.displayLaps = 'none';
     this.laps = [];
   }
 
   markLap() {
-    this.titleLaps = false;
-    this.displayLaps = 'inline-block';
     this.laps.push(this.timeLeft);
   }
 
